c0df8d19bbc4cef0d6b807b27315c67916841d2e
ornan-matos <ornan.matos@giusoft.com.br>
2025-10-01 15:16:31 -0300

    update:ativar linhas fixas, ativar quebra de linha

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,14 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+--
+-- Ativar quebra de linha automática
+vim.opt.wrap = true
+
+-- Opcional: melhorar a experiência de navegação em linhas longas
+vim.opt.linebreak = true -- evita quebrar no meio das palavras
+vim.opt.showbreak = "↪ " -- indicador visual para linha quebrada
+
+-- Mostrar números de linha absolutos
+vim.opt.number = true
+vim.opt.relativenumber = false -- desativar numeração dinâmica
